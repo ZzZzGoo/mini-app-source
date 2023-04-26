39482c5c265179321c8f678c99ca70edd2b9ef51
Vazghen Vardanian <vazgen.eth@gmail.com>
2023-04-26 10:24:52 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const links = [
+  {
+    id: 1,
+    url: 'https://zzzzgoo.github.io/proff-nav/',
+    text: 'Navbar',
+  },
+  {
+    id: 2,
+    url: 'https://zzzzgoo.github.io/sidebar/',
+    text: 'Sidebar',
+  },
+  {
+    id: 3,
+    url: 'https://vazghens-markdown-preview-mini-app.netlify.app/',
+    text: 'Markdown',
+  },
+  {
+    id: 4,
+    url: 'https://zzzzgoo.github.io/contact-form-ex/',
+    text: 'Login',
+  },
+  {
+    id: 5,
+    url: 'https://vazghens-accordion-mini-app.netlify.app/',
+    text: 'Accordion',
+  },
+  {
+    id: 6,
+    url: 'https://vazghens-cart-mini-app.netlify.app/',
+    text: 'Cart',
+  },
+  {
+    id: 7,
+    url: 'https://vazghens-cocktails-mini-app.netlify.app/',
+    text: 'Cocktails',
+  },
+  {
+    id: 8,
+    url: 'https://vazghens-grocery-bud-mini-app.netlify.app/',
+    text: 'Grocery',
+  },
+  {
+    id: 9,
+    url: 'https://vazghens-movie-mini-app.netlify.app/',
+    text: 'Movie',
+  },
+  {
+    id: 10,
+    url: 'https://vazghens-news-mini-app.netlify.app/',
+    text: 'News',
+  },
+  {
+    id: 11,
+    url: 'https://vazghens-pagination-mini-app.netlify.app/',
+    text: 'Pagination',
+  },
+  {
+    id: 12,
+    url: 'https://vazghens-quiz-mini-app.netlify.app/',
+    text: 'Quiz',
+  },
+  {
+    id: 13,
+    url: 'https://vazghens-reviews-mini-app.netlify.app/',
+    text: 'Reviews',
+  },
+  {
+    id: 14,
+    url: 'https://vazghens-tours-mini-app.netlify.app/',
+    text: 'Tours',
+  },
+  {
+    id: 15,
+    url: 'https://vazghens-dark-mode-mini-app.netlify.app/',
+    text: 'Dark',
+  },
+];
